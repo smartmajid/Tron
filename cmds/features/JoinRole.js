bd5db19409fadfdbf49deee5589a87e70e02baf8
Devin Leaman <dleaman212@gmail.com>
2018-10-18 03:41:46 -0500

    Added latest changes (WIP)

--- cmds/features/JoinRole.js
+++ cmds/features/JoinRole.js
@@ -20,20 +20,15 @@ class JoinRole extends Command {
   }
 
   async run (msg, { role }) {
-    mongo.isRoleAvailable(msg.guild.id, role.name).then(available => {
+    try {
+      const available = await mongo.isRoleAvailable(msg.guild.id, role.name)
       if (available) {
-        console.log(msg.member)
-        msg.member.addRole(role.id).then(member => {
-          msg.channel.send(`You've successfully been added to the ${role.name} role!`)
-        }).catch(err => {
-          if (err.code === 50013) {
-            msg.channel.send('It appears that I don\'t have permission to give you that role. Please contact the server administrator to fix this.')
-          }
-
-          console.error(err)
-        })
-      } else msg.channel.send('D\'awww, it isn\'t available.')
-    })
+        if (msg.member.manageable) {
+          const res = await msg.member.edit({ roles: [role.id] }, 'User requested.')
+          console.log(res)
+        } else return msg.reply('Unfortunately, I do not have permission to edit this users roles.')
+      } else return msg.channel.send('D\'awww, the specified role isn\'t available.')
+    } catch (err) { console.error(err) }
   }
 }
 
